db7225e1f2228d72961b1a1d8f5df688ab7f54c2
Osef1712 <j.zumkir@ludus-academie.com>
2021-07-20 12:48:45 +0200

    ajout_dernière_page

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,26 +1,56 @@
-var arrayTheme = [
-	['Home', '#Home'],
-	['Game Design', '#GD'],
-	['Graphisme', '#Graph'],
-	['Programmation', '#Prog']
-]
+var langChecked = document.getElementById("check_lang").checked;
 
-var arrayGD = [
-	['Nourriture Rapide', 'GD_NourritureRapide.html'],
-	['10 pagers', 'GD_10Oagers.html']
-]
 
-var arrayGraph = [
-	['Pixel Art','Graph_PixelArt.html'],
-	['Cyber Drunk','Graph_CyberDrunk.html'],
-	['Chaos User','Graph_ChaosUser.html']
-]
+if(langChecked){
+	var arrayGD = [
+		['Nourriture Rapide', 'FR_NourritureRapide.html']/*,
+		['10 Pagers', 'FR_10Pagers.html']*/
+	]
 
-var arrayProg = [
-	['Old Memories', 'Prog_OldMemories'],
-	['Liminal Museum', 'Prog_LiminalMuseum'],
-	['The 4 Suits', 'Prog_The4Suits']
-]
+	var arrayGraph = [
+		/*['Pixel Art','FR_PixelArt.html'],*/
+		['Cyber Drunk','FR_CyberDrunk.html']/*,
+		['Chaos User','FR_ChaosUser.html']*/
+	]
+
+	var arrayProg = [
+		['Ping Fight', 'FR_PingFight.html'],
+		['Liminal Museum', 'FR_LiminalMuseum.html'],
+		['The 4 Suits', 'FR_The4Suits.html']
+	]
+
+	var arrayTheme = [
+		['Home', '#Home'],
+		['Game Design', '#GD'],
+		['Graphisme', '#Graph'],
+		['Programmation', '#Prog']
+	]
+}else{
+	var arrayGD = [
+		['Nourriture Rapide', 'EN_NourritureRapide.html']/*,
+		['10 Pagers', 'EN_10Pagers.html']*/
+	]
+
+	var arrayGraph = [
+		/*['Pixel Art','EN_PixelArt.html'],*/
+		['Cyber Drunk','EN_CyberDrunk.html']/*,
+		['Chaos User','EN_ChaosUser.html']*/
+	]
+
+	var arrayProg = [
+		['Ping Fight', 'EN_PingFight.html'],
+		['Liminal Museum', 'EN_LiminalMuseum.html'],
+		['The 4 Suits', 'EN_The4Suits.html']
+	]
+	var arrayTheme = [
+		['Home', '#Home'],
+		['Game Design', '#GD'],
+		['Graphics', '#Graph'],
+		['Programming', '#Prog']
+	]
+}
+
+
 
 let navBar = document.getElementById('nav');
 let ulBar = document.createElement("ul");
@@ -41,11 +71,12 @@ for (var i = 0; i < arrayTheme.length; i++) {
 	if (i!=0) {
 		aLiBar.innerHTML = arrayTheme[i][0]+"<span>&rsaquo;</span>";
 	}else{
-		if (document.title == "Home") {
+		/*if (document.title == "Home") {
 			aLiBar.setAttribute("href","./index.html");
 		}else{
 			aLiBar.setAttribute("href","../index.html");
-		}
+		}*/
+		aLiBar.setAttribute("href","./FR_Intro.html");
 		aLiBar.innerHTML = arrayTheme[i][0];
 	}
 	liBar.appendChild(aLiBar);
@@ -53,6 +84,8 @@ for (var i = 0; i < arrayTheme.length; i++) {
 	if(i!=0){
 		let ulUlBar = document.createElement("ul");
 		ulUlBar.setAttribute("id", "UL"+i);
+		ulUlBar.classList.add(".w3-animate-opacity");
+
 		liBar.appendChild(ulUlBar);
 	}
 
@@ -72,11 +105,12 @@ function addPage(theUl,theLi,theArray){
 		var newLi = document.createElement("li");
 		theUl3.appendChild(newLi);
 		var newA = document.createElement("a");
-		if(document.title == "Home"){
+		/*if(document.title == "Home"){
 			newA.setAttribute("href", "./HTML/"+theArray[i][1]);
 		}else{
 			newA.setAttribute("href", "../HTML/"+theArray[i][1]);
-		}
+		}*/
+		newA.setAttribute("href", "./"+theArray[i][1]);
 		newA.innerHTML = theArray[i][0];
 		newLi.appendChild(newA);
 
@@ -85,5 +119,16 @@ function addPage(theUl,theLi,theArray){
 			document.getElementById(theLi).classList.add("current");
 			document.getElementById("LI0").classList.remove("current");
 		}
+	}
+}
+
+function changeLang(){
+	/*var currentPage = document.title;
+	var lang = currentPage.substr(0,2);
+	console.log(lang);*/
+	if(langChecked){
+		window.location.href = "EN_"+document.title.split(" ").join("")+".html";
+	}else{
+		window.location.href = "FR_"+document.title.split(" ").join("")+".html";
 	}
 }
